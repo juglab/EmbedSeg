44efef343b8a2d097f323d52bef7df5e502cfd2e
mlbyml <manan.lalit@gmail.com>
2021-01-24 20:41:25 +0100

    Add image titles on the top left

--- EmbedSeg/utils/visualize.py
+++ EmbedSeg/utils/visualize.py
@@ -19,23 +19,30 @@ def create_color_map(n_colors= 10):
     return newcmp
 
 def visualize(image, prediction, ground_truth, embedding, new_cmp):
+    font = {'family': 'serif',
+        'color':  'white',
+        'weight': 'bold',
+        'size': 16,
+        }
     plt.figure(figsize=(15,15))
     img_show = image if image.ndim==2 else image[...,0]
     plt.subplot(221); 
     plt.imshow(img_show, cmap='magma'); 
+    plt.text(20, 20, "IM", fontdict=font)
     plt.xlabel('Image')
     plt.axis('off')
     plt.subplot(222); 
     plt.axis('off')
     plt.imshow(ground_truth, cmap=new_cmp, interpolation = 'None')
+    plt.text(20, 20, "GT", fontdict=font)
     plt.xlabel('Ground Truth')
     plt.subplot(223);
     plt.axis('off')
     plt.imshow(embedding,  interpolation = 'None')
-    
     plt.subplot(224);  
     plt.axis('off')
     plt.imshow(prediction, cmap=new_cmp, interpolation = 'None')
+    plt.text(20, 20, "PRED", fontdict=font)
     plt.xlabel('Prediction')
     plt.tight_layout()
     plt.show()
