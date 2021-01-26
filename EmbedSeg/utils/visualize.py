c6e9325f5a143afefcf56a6b4b08f0578148466a
mlbyml <manan.lalit@gmail.com>
2021-01-26 04:21:47 +0100

    Move the image title slightly lower

--- EmbedSeg/utils/visualize.py
+++ EmbedSeg/utils/visualize.py
@@ -28,13 +28,13 @@ def visualize(image, prediction, ground_truth, embedding, new_cmp):
     img_show = image if image.ndim==2 else image[...,0]
     plt.subplot(221); 
     plt.imshow(img_show, cmap='magma'); 
-    plt.text(20, 20, "IM", fontdict=font)
+    plt.text(30, 30, "IM", fontdict=font)
     plt.xlabel('Image')
     plt.axis('off')
     plt.subplot(222); 
     plt.axis('off')
     plt.imshow(ground_truth, cmap=new_cmp, interpolation = 'None')
-    plt.text(20, 20, "GT", fontdict=font)
+    plt.text(30, 30, "GT", fontdict=font)
     plt.xlabel('Ground Truth')
     plt.subplot(223);
     plt.axis('off')
@@ -42,7 +42,7 @@ def visualize(image, prediction, ground_truth, embedding, new_cmp):
     plt.subplot(224);  
     plt.axis('off')
     plt.imshow(prediction, cmap=new_cmp, interpolation = 'None')
-    plt.text(20, 20, "PRED", fontdict=font)
+    plt.text(30, 30, "PRED", fontdict=font)
     plt.xlabel('Prediction')
     plt.tight_layout()
     plt.show()
