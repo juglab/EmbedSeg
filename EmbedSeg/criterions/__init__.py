07a3ce5d7a867bc73559ccc0641d0b48124bc9f5
mlbyml <manan.lalit@gmail.com>
2021-03-07 17:57:14 +0100

    Update `__init__.py` to import 3d-relevant code

--- EmbedSeg/criterions/__init__.py
+++ EmbedSeg/criterions/__init__.py
@@ -1,6 +1,11 @@
 from EmbedSeg.criterions.my_loss import SpatialEmbLoss
+from EmbedSeg.criterions.my_loss_3d import SpatialEmbLoss_3d
 
 
-def get_loss(grid_y, grid_x, pixel_y, pixel_x, one_hot, loss_opts):
-    return SpatialEmbLoss(grid_y, grid_x, pixel_y, pixel_x, one_hot, **loss_opts)
+
+def get_loss(grid_z, grid_y, grid_x, pixel_z, pixel_y, pixel_x, one_hot, loss_opts):
+    if grid_z is None:
+        return SpatialEmbLoss(grid_y, grid_x, pixel_y, pixel_x, one_hot, **loss_opts)
+    else:
+        return SpatialEmbLoss_3d(grid_z, grid_y, grid_x, pixel_z, pixel_y, pixel_x, one_hot, **loss_opts)
     
