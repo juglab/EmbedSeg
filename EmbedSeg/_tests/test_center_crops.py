f7c17b2cf80c89bcdf2abd9908fe094d5eb74c4e
Ubuntu <ubuntu@dynamicdarwin-0133d.openstack.bielefeld>
2021-05-05 12:00:44 +0000

    Add `test_center_crops.py`

---
+++ EmbedSeg/_tests/test_center_crops.py
@@ -0,0 +1,31 @@
+from EmbedSeg.utils.generate_crops import generate_center_image, generate_center_image_3d
+import pytest
+import numpy as np
+
+def test_generate_center_image_1():
+    ma = np.zeros((100, 100))
+    ma [30:-30, 30:-30] = 2
+    ids = np.unique(ma)
+    ids = ids[ids!=0]
+    center_image = generate_center_image(ma, center='centroid', ids=ids, one_hot = False )
+    y, x = np.where(center_image==True)
+    assert y==50 and x==50
+
+def test_generate_center_image_2():
+    ma = np.zeros((100, 100))
+    ma [30:-30, 30:-30] = 2
+    ids = np.unique(ma)
+    ids = ids[ids!=0]
+    center_image = generate_center_image(ma, center='medoid', ids=ids, one_hot = False )
+    y, x = np.where(center_image==True)
+    assert np.abs(y-50)<=1 and np.abs(x-50)<=1    
+
+
+def test_generate_center_image_3():
+    ma = np.zeros((100, 100, 100))
+    ma [30:-30, 30:-30, 30:-30] = 2
+    ids = np.unique(ma)
+    ids = ids[ids!=0]
+    center_image = generate_center_image_3d(ma, center='medoid', ids=ids, one_hot = False, anisotropy_factor=1, speed_up=1)
+    z, y, x = np.where(center_image==True)
+    assert np.abs(z-50)<=1 and np.abs(y-50)<=1 and np.abs(x-50)<=1
