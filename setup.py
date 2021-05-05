5092bcecc9c78157eb40728d6c6e685fea6be2b4
Ubuntu <ubuntu@dynamicdarwin-0133d.openstack.bielefeld>
2021-05-05 11:13:57 +0000

    Add additional classifiers

--- setup.py
+++ setup.py
@@ -18,7 +18,11 @@ setup(
         "Bug Tracker": "https://github.com/juglab/EmbedSeg/issues",
     },
     classifiers=[
-        "Programming Language :: Python :: 3",
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
