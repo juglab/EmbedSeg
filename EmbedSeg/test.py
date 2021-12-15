222032bb8b3247cf9f681267975bd6ddb6751fc7
mlbyml <manan.lalit@gmail.com>
2021-12-15 11:53:41 +0100

    Add `test_3d_sliced` method to `test.py`

--- EmbedSeg/test.py
+++ EmbedSeg/test.py
@@ -68,7 +68,13 @@ def begin_evaluating(test_configs, verbose=True, mask_region=None):
         test_3d(verbose=verbose,
                 grid_x=test_configs['grid_x'], grid_y=test_configs['grid_y'], grid_z=test_configs['grid_z'],
                 pixel_x=test_configs['pixel_x'], pixel_y=test_configs['pixel_y'], pixel_z=test_configs['pixel_z'],
-                one_hot=test_configs['dataset']['kwargs']['one_hot'], mask_region=mask_region)
+                one_hot=test_configs['dataset']['kwargs']['one_hot'], mask_region=mask_region, n_sigma = n_sigma)
+    elif (test_configs['name'] == '3d_sliced'):
+        test_3d_sliced(verbose=verbose,
+                       grid_x=test_configs['grid_x'], grid_y=test_configs['grid_y'], grid_z=test_configs['grid_z'],
+                       pixel_x=test_configs['pixel_x'], pixel_y=test_configs['pixel_y'],
+                       pixel_z=test_configs['pixel_z'],
+                       one_hot=test_configs['dataset']['kwargs']['one_hot'], mask_region=mask_region, n_sigma = n_sigma)
 
 
 def test(verbose, grid_y=1024, grid_x=1024, pixel_y=1, pixel_x=1, one_hot=False, n_sigma=2):
@@ -122,8 +128,6 @@ def test(verbose, grid_y=1024, grid_x=1024, pixel_y=1, pixel_x=1, one_hot=False,
                                                         min_mask_sum=min_mask_sum,
                                                         min_unclustered_sum=min_unclustered_sum,
                                                         min_object_size=min_object_size)
-
-
 
             center_x, center_y, samples_x, samples_y, sample_spatial_embedding_x, sample_spatial_embedding_y, sigma_x, sigma_y, \
             color_sample_dic, color_embedding_dic = prepare_embedding_for_test_image(instance_map=instance_map,
@@ -228,7 +232,7 @@ def test(verbose, grid_y=1024, grid_x=1024, pixel_y=1, pixel_x=1, one_hot=False,
 
 
 def test_3d(verbose, grid_x=1024, grid_y=1024, grid_z=32, pixel_x=1, pixel_y=1, pixel_z=1, one_hot=False,
-            mask_region=None):
+            mask_region=None, n_sigma = 3):
     """
     mask_region: list of two lists
                 first list contains starting coordinates of region which needs to be masked in z, y and x ordering fashion
@@ -274,7 +278,7 @@ def test_3d(verbose, grid_x=1024, grid_y=1024, grid_z=32, pixel_x=1, pixel_y=1, 
                         output_average = apply_tta_3d(im, model, iter)  # iter
                     else:
                         output_average = 1 / (iter + 1) * (
-                                    output_average * iter + apply_tta_3d(im, model, iter))  # iter
+                                output_average * iter + apply_tta_3d(im, model, iter))  # iter
                 output = torch.from_numpy(output_average).float().cuda()
             else:
                 output = model(im)
@@ -288,15 +292,209 @@ def test_3d(verbose, grid_x=1024, grid_y=1024, grid_z=32, pixel_x=1, pixel_y=1, 
                                                         )
 
             # unpad instance_map, instances and images
-            if diff_z//2 is not 0 and (diff_z-diff_z//2) is not 0:
-                instance_map = instance_map[diff_z//2:-(diff_z-diff_z//2), ...]
-                instances = instances[diff_z//2:-(diff_z-diff_z//2), ...]
-                im = im[:, :, diff_z//2:-(diff_z-diff_z//2), ...]
-            if diff_y//2 is not 0 and (diff_y-diff_y//2) is not 0:
+            if diff_z // 2 is not 0 and (diff_z - diff_z // 2) is not 0:
+                instance_map = instance_map[diff_z // 2:-(diff_z - diff_z // 2), ...]
+                instances = instances[diff_z // 2:-(diff_z - diff_z // 2), ...]
+                im = im[:, :, diff_z // 2:-(diff_z - diff_z // 2), ...]
+            if diff_y // 2 is not 0 and (diff_y - diff_y // 2) is not 0:
                 instance_map = instance_map[:, diff_y // 2:-(diff_y - diff_y // 2), ...]
                 instances = instances[:, diff_y // 2:-(diff_y - diff_y // 2), ...]
                 im = im[:, :, :, diff_y // 2:-(diff_y - diff_y // 2), ...]
-            if diff_x//2 is not 0 and (diff_x-diff_x//2) is not 0:
+            if diff_x // 2 is not 0 and (diff_x - diff_x // 2) is not 0:
+                instance_map = instance_map[..., diff_x // 2:-(diff_x - diff_x // 2)]
+                instances = instances[..., diff_x // 2:-(diff_x - diff_x // 2)]
+                im = im[..., diff_x // 2:-(diff_x - diff_x // 2)]
+
+            if (mask_region is not None):
+                # ignore predictions in this region prior to saving the tiffs or prior to comparison with GT masks
+                instance_map[int(mask_region[0][0]):int(mask_region[1][0]),
+                int(mask_region[0][1]):int(mask_region[1][1]),
+                int(mask_region[0][2]):int(mask_region[1][2])] = 0  # Z Y X
+            else:
+                pass
+
+            if (one_hot):
+                if ('instance' in sample):
+                    sc = obtain_AP_one_hot(gt_image=instances.cpu().detach().numpy(),
+                                           prediction_image=instance_map.cpu().detach().numpy(), ap_val=ap_val)
+                    if (verbose):
+                        print("Accuracy: {:.03f}".format(sc), flush=True)
+                    resultList.append(sc)
+            else:
+                if ('instance' in sample):
+                    sc = matching_dataset(y_true=[instances.cpu().detach().numpy()],
+                                          y_pred=[instance_map.cpu().detach().numpy()], thresh=ap_val,
+                                          show_progress=False)
+                    if (verbose):
+                        print("Accuracy: {:.03f}".format(sc.accuracy), flush=True)
+                    resultList.append(sc.accuracy)
+
+            if save_images and ap_val == 0.5:
+                if not os.path.exists(os.path.join(save_dir, 'predictions/')):
+                    os.makedirs(os.path.join(save_dir, 'predictions/'))
+                    print("Created new directory {}".format(os.path.join(save_dir, 'predictions/')))
+                if not os.path.exists(os.path.join(save_dir, 'ground-truth/')):
+                    os.makedirs(os.path.join(save_dir, 'ground-truth/'))
+                    print("Created new directory {}".format(os.path.join(save_dir, 'ground-truth/')))
+                if not os.path.exists(os.path.join(save_dir, 'seeds/')):
+                    os.makedirs(os.path.join(save_dir, 'seeds/'))
+                    print("Created new directory {}".format(os.path.join(save_dir, 'seeds/')))
+                if not os.path.exists(os.path.join(save_dir, 'images/')):
+                    os.makedirs(os.path.join(save_dir, 'images/'))
+                    print("Created new directory {}".format(os.path.join(save_dir, 'images/')))
+
+                base, _ = os.path.splitext(os.path.basename(sample['im_name'][0]))
+                imageFileNames.append(base)
+
+                instances_file = os.path.join(save_dir, 'predictions/', base + '.tif')
+                imsave(instances_file, instance_map.cpu().detach().numpy().astype(np.uint16))
+                if ('instance' in sample):
+                    gt_file = os.path.join(save_dir, 'ground-truth/', base + '.tif')
+                    imsave(gt_file, instances.cpu().detach().numpy().astype(np.uint16))
+
+                seeds_file = os.path.join(save_dir, 'seeds/', base + '.tif')
+                imsave(seeds_file, torch.sigmoid(output[0, -1, ...]).cpu().detach().numpy())
+
+                im_file = os.path.join(save_dir, 'images/', base + '.tif')
+                imsave(im_file, im[0, 0].cpu().detach().numpy())
+
+        if save_results and 'instance' in sample:
+            if not os.path.exists(os.path.join(save_dir, 'results/')):
+                os.makedirs(os.path.join(save_dir, 'results/'))
+                print("Created new directory {}".format(os.path.join(save_dir, 'results/')))
+            txt_file = os.path.join(save_dir,
+                                    'results/combined_AP-' + '{:.02f}'.format(ap_val) + '_tta-' + str(tta) + '.txt')
+            with open(txt_file, 'w') as f:
+                f.writelines(
+                    "image_file_name, min_mask_sum, min_unclustered_sum, min_object_size, seed_thresh, intersection_threshold, accuracy \n")
+                f.writelines("+++++++++++++++++++++++++++++++++\n")
+                for ind, im_name in enumerate(imageFileNames):
+                    im_name_png = im_name + '.png'
+                    score = resultList[ind]
+                    f.writelines(
+                        "{} {:.02f} {:.02f} {:.02f} {:.02f} {:.02f} {:.05f} \n".format(im_name_png, min_mask_sum,
+                                                                                       min_unclustered_sum,
+                                                                                       min_object_size, seed_thresh,
+                                                                                       ap_val, score))
+                f.writelines("+++++++++++++++++++++++++++++++++\n")
+                f.writelines("Average Precision (AP)  {:.02f} {:.05f}\n".format(ap_val, np.mean(resultList)))
+
+            print(
+                "Mean Average Precision at IOU threshold = {}, is equal to {:.05f}".format(ap_val, np.mean(resultList)))
+
+
+def test_3d_sliced(verbose, grid_x=1024, grid_y=1024, grid_z=32, pixel_x=1, pixel_y=1, pixel_z=1, one_hot=False,
+            mask_region=None, n_sigma = 3):
+    """
+    mask_region: list of two lists
+                first list contains starting coordinates of region which needs to be masked in z, y and x ordering fashion
+                second list contains ending coordinates of region which needs to be masked in z, y and x ordering fashion
+    """
+    model.eval()
+    # cluster module
+    cluster = Cluster_3d(grid_z, grid_y, grid_x, pixel_z, pixel_y, pixel_x)
+
+    with torch.no_grad():
+        resultList = []
+        imageFileNames = []
+        for sample in tqdm(dataset_it):
+            im = sample['image']
+
+            multiple_z = im.shape[2] // 8
+            multiple_y = im.shape[3] // 8
+            multiple_x = im.shape[4] // 8
+
+            if im.shape[2] % 8 != 0:
+                diff_z = 8 * (multiple_z + 1) - im.shape[2]
+            else:
+                diff_z = 0
+            if im.shape[3] % 8 != 0:
+                diff_y = 8 * (multiple_y + 1) - im.shape[3]
+            else:
+                diff_y = 0
+            if im.shape[4] % 8 != 0:
+                diff_x = 8 * (multiple_x + 1) - im.shape[4]
+            else:
+                diff_x = 0
+            p3d = (diff_x // 2, diff_x - diff_x // 2, diff_y // 2, diff_y - diff_y // 2, diff_z // 2,
+                   diff_z - diff_z // 2)  # last dim, second last dim, third last dim!
+
+            im = F.pad(im, p3d, "reflect")
+            if ('instance' in sample):
+                instances = sample['instance'].squeeze()
+                instances = F.pad(instances, p3d, "constant", 0)
+
+            print("Processing `YX` slices .....")
+            # obtain YX slices
+            output_YX_3d = torch.zeros(1, 5, im.shape[2], im.shape[3], im.shape[4]).cuda()
+            for z in tqdm(range(im.shape[2]), position=0, leave=True):
+                if (tta):
+                    output_YX = apply_tta_2d(im[:, :, z, ...], model)
+                else:
+                    output_YX = model(im[:, :, z, ...])
+                output_YX_3d[:, :, z, ...] = output_YX
+            print("Processing `ZX` slices .....")
+            # obtain ZX slices
+            output_ZX_3d = torch.zeros(1, 5, im.shape[2], im.shape[3], im.shape[4]).cuda()
+            for y in tqdm(range(im.shape[3]), position=0, leave=True):
+                if (tta):
+                    output_ZX = apply_tta_2d(im[:, :, :, y, ...], model)
+                else:
+                    output_ZX = model(im[:, :, :, y, ...])
+                # offset in x, offset in z, sigma in x, sigma in z, seed
+                output_ZX_3d[:, :, :, y, ...] = output_ZX
+            print("Processing `ZY` slices .....")
+            # obtain ZY slices
+            output_ZY_3d = torch.zeros(1, 5, im.shape[2], im.shape[3], im.shape[4]).cuda()
+            for x in tqdm(range(im.shape[4]), position=0, leave=True):
+                if (tta):
+                    output_ZY = apply_tta_2d(im[:, :, :, :, x], model)
+                else:
+                    output_ZY = model(im[:, :, :, :, x])
+
+                # offset in y, offset in z, sigma in y, sigma in z, seed
+                output_ZY_3d[:, :, :, :, x] = output_ZY
+            output = torch.from_numpy(np.zeros((1, 7, im.shape[2], im.shape[3], im.shape[4]))).float().cuda()
+
+            # handle seediness
+            output[:, 6, ...] = 1/3*(output_YX_3d[:, 4, ...]+ output_ZX_3d[:, 4, ...] + output_ZY_3d[:, 4, ...])
+
+            # handle offset in X
+            output[:, 0, ...] = 1/2*(output_YX_3d[:, 0, ...] + output_ZX_3d[:, 0, ...])
+
+            # handle offset in Y
+            output[:, 1, ...] = 1/2*(output_YX_3d[:, 1, ...] + output_ZY_3d[:, 0, ...])
+
+            # handle offset in Z
+            output[:, 2, ...] = 1/2 * (output_ZX_3d[:, 1, ...] + output_ZY_3d[:, 1, ...])
+
+            # sigma in X
+            output[:, 3, ...] = 1/2 * (output_YX_3d[:, 2, ...] + output_ZX_3d[:, 2, ...])
+
+            # sigma in Y
+            output[:, 4, ...] = 1/2*(output_YX_3d[:, 3, ...] + output_ZY_3d[:, 2, ...])
+
+            # sigma in Z
+            output[:, 5, ...] = 1/2*(output_ZX_3d[:, 3, ...] + output_ZY_3d[:, 3, ...])
+
+            instance_map, predictions = cluster.cluster(output[0],
+                                                        n_sigma=n_sigma, # 3
+                                                        seed_thresh=seed_thresh,
+                                                        min_mask_sum=min_mask_sum,
+                                                        min_unclustered_sum=min_unclustered_sum,
+                                                        min_object_size=min_object_size,
+                                                        )
+
+            # unpad instance_map, instances and images
+            if diff_z // 2 is not 0 and (diff_z - diff_z // 2) is not 0:
+                instance_map = instance_map[diff_z // 2:-(diff_z - diff_z // 2), ...]
+                instances = instances[diff_z // 2:-(diff_z - diff_z // 2), ...]
+                im = im[:, :, diff_z // 2:-(diff_z - diff_z // 2), ...]
+            if diff_y // 2 is not 0 and (diff_y - diff_y // 2) is not 0:
+                instance_map = instance_map[:, diff_y // 2:-(diff_y - diff_y // 2), ...]
+                instances = instances[:, diff_y // 2:-(diff_y - diff_y // 2), ...]
+                im = im[:, :, :, diff_y // 2:-(diff_y - diff_y // 2), ...]
+            if diff_x // 2 is not 0 and (diff_x - diff_x // 2) is not 0:
                 instance_map = instance_map[..., diff_x // 2:-(diff_x - diff_x // 2)]
                 instances = instances[..., diff_x // 2:-(diff_x - diff_x // 2)]
                 im = im[..., diff_x // 2:-(diff_x - diff_x // 2)]
