8dfb2de1ba649776095ca6367c3b1647af6c41ed
mlbyml <manan.lalit@gmail.com>
2022-09-17 14:06:44 +0200

    Add doc strings to `utils.py`

--- EmbedSeg/utils/utils.py
+++ EmbedSeg/utils/utils.py
@@ -109,7 +109,77 @@ class Visualizer:
 
 
 class Cluster_3d:
+    """
+            A class used to cluster pixel embeddings in 3D
+
+            Attributes
+            ----------
+            xyzm :  float (3, W, D, H)
+                    pixel coordinates of tile /grid
+
+            one_hot : bool
+                    Should be set to True, if the GT label masks are present in a one-hot encoded fashion
+                    Not applicable to 3D. This parameter will be deprecated in a future release
+
+            grid_x: int
+                    Length (width) of grid / tile
+
+            grid_y: int
+                    Height of grid / tile
+
+            grid_z: int
+                    Depth of grid / tile
+
+            pixel_x: float
+                    if grid_x = 1000 and pixel_x = 1.0, then the pixel spacing along the x direction is pixel_x/grid_x = 1e-3
+            pixel_y: float
+                    if grid_y = 1000 and pixel_y = 1.0, then the pixel spacing along the y direction is pixel_y/grid_y = 1e-3
+            pixel_z: float
+                    if grid_z = 1000 and pixel_z = 1, then the pixel spacing along the z direction is pixel_z/grid_z = 1e-3
+
+
+            Methods
+            -------
+            __init__: Initializes an object of class `Cluster_3d`
+
+            cluster_with_gt: use the predicted spatial embeddings from all pixels belonging to the GT label mask
+                        to identify the predicted cluster (used during training and validation)
+
+            cluster:    use the  predicted spatial embeddings from all pixels in the test image.
+                        Employs `fg_thresh` and `seed_thresh`
+            cluster_local_maxima: use the  predicted spatial embeddings from all pixels in the test image.
+                        Employs only `fg_thresh`
+            """
+
     def __init__(self, grid_z, grid_y, grid_x, pixel_z, pixel_y, pixel_x, one_hot=False):
+        """
+           Parameters
+           ----------
+           xyzm :  float (3, W, D, H)
+                    pixel coordinates of tile /grid
+
+            one_hot : bool
+                    Should be set to True, if the GT label masks are present in a one-hot encoded fashion
+                    Not applicable to 3D. This parameter will be deprecated in a future release
+
+            grid_x: int
+                    Length (width) of grid / tile
+
+            grid_y: int
+                    Height of grid / tile
+
+            grid_z: int
+                    Depth of grid / tile
+
+            pixel_x: float
+                    if grid_x = 1000 and pixel_x = 1.0, then the pixel spacing along the x direction is pixel_x/grid_x = 1e-3
+            pixel_y: float
+                    if grid_y = 1000 and pixel_y = 1.0, then the pixel spacing along the y direction is pixel_y/grid_y = 1e-3
+            pixel_z: float
+                    if grid_z = 1000 and pixel_z = 1, then the pixel spacing along the z direction is pixel_z/grid_z = 1e-3
+
+           """
+
         xm = torch.linspace(0, pixel_x, grid_x).view(1, 1, 1, -1).expand(1, grid_z, grid_y, grid_x)
         ym = torch.linspace(0, pixel_y, grid_y).view(1, 1, -1, 1).expand(1, grid_z, grid_y, grid_x)
         zm = torch.linspace(0, pixel_z, grid_z).view(1, -1, 1, 1).expand(1, grid_z, grid_y, grid_x)
@@ -124,18 +194,34 @@ class Cluster_3d:
         self.pixel_y = pixel_y
         self.pixel_z = pixel_z
 
-    def cluster_with_gt(self, prediction, instance, n_sigma=1, ):
+    def cluster_with_gt(self, prediction, gt_instance, n_sigma=3, ):
+        """
+        Parameters
+        ----------
+        prediction :  PyTorch Tensor
+                Model Prediction (7, D, H, W)
+        gt_instance : PyTorch Tensor
+                Ground Truth Instance Segmentation Label Map
+
+        n_sigma: int, default = 3
+                Number of dimensions in Raw Image
+        Returns
+        ----------
+        instance: PyTorch Tensor (D, H, W)
+                instance segmentation
+       """
+
         depth, height, width = prediction.size(1), prediction.size(2), prediction.size(3)
         xyzm_s = self.xyzm[:, 0:depth, 0:height, 0:width]  # 3 x d x h x w
         spatial_emb = torch.tanh(prediction[0:3]) + xyzm_s  # 3 x d x h x w
         sigma = prediction[3:3 + n_sigma]  # n_sigma x h x w
 
         instance_map = torch.zeros(depth, height, width).short().cuda()
-        unique_instances = instance.unique()
+        unique_instances = gt_instance.unique()
         unique_instances = unique_instances[unique_instances != 0]
 
         for id in unique_instances:
-            mask = instance.eq(id).view(1, depth, height, width)
+            mask = gt_instance.eq(id).view(1, depth, height, width)
             center = spatial_emb[mask.expand_as(spatial_emb)].view(
                 3, -1).mean(1).view(3, 1, 1, 1)  # 3 x 1 x 1 x 1
             s = sigma[mask.expand_as(sigma)].view(n_sigma, -1).mean(1).view(n_sigma, 1, 1, 1)
@@ -149,6 +235,31 @@ class Cluster_3d:
 
     def cluster(self, prediction, n_sigma=3, seed_thresh=0.9, fg_thresh=0.5, min_mask_sum=0, min_unclustered_sum=0,
                 min_object_size=36):
+
+        """
+                Parameters
+                ----------
+                prediction :  PyTorch Tensor
+                        Model Prediction (7, D, H, W)
+                n_sigma: int, default = 3
+                        Number of dimensions in Raw Image
+                seed_thresh : float, default=0.9
+                        Seediness Threshold defines which pixels are considered to identify object centres
+                fg_thresh: float, default=0.5
+                        Foreground Threshold defines which pixels are considered to the form the Foreground
+                        and which would need to be clustered into unique objects
+                min_mask_sum: int
+                        Only start creating instances, if there are at least `min_mask_sum` pixels in foreground!
+                min_unclustered_sum: int
+                        Stop when the number of seed candidates are less than `min_unclustered_sum`
+                min_object_size: int
+                    Predicted Objects below this threshold are ignored
+
+                Returns
+                ----------
+                instance: PyTorch Tensor (D, H, W)
+                        instance segmentation
+               """
 
         depth, height, width = prediction.size(1), prediction.size(2), prediction.size(3)
         xyzm_s = self.xyzm[:, 0:depth, 0:height, 0:width]
@@ -195,8 +306,32 @@ class Cluster_3d:
 
         return instance_map
 
-    def cluster_local_maxima(self, prediction, n_sigma=3, fg_thresh=0.5, min_mask_sum=128, min_unclustered_sum=0,
+    def cluster_local_maxima(self, prediction, n_sigma=3, fg_thresh=0.5, min_mask_sum=0, min_unclustered_sum=0,
                              min_object_size=36):
+
+        """
+            Parameters
+            ----------
+            prediction :  PyTorch Tensor
+                    Model Prediction (7, D, H, W)
+            n_sigma: int, default = 3
+                    Number of dimensions in Raw Image
+            fg_thresh: float, default=0.5
+                    Foreground Threshold defines which pixels are considered to the form the Foreground
+                    and which would need to be clustered into unique objects
+            min_mask_sum: int
+                    Only start creating instances, if there are at least `min_mask_sum` pixels in foreground!
+            min_unclustered_sum: int
+                    Stop when the number of seed candidates are less than `min_unclustered_sum`
+            min_object_size: int
+                Predicted Objects below this threshold are ignored
+
+            Returns
+            ----------
+            instance: PyTorch Tensor (D, H, W)
+                    instance segmentation
+           """
+
         from scipy.ndimage import gaussian_filter
         depth, height, width = prediction.size(1), prediction.size(2), prediction.size(3)
         xyzm_s = self.xyzm[:, 0:depth, 0:height, 0:width]
@@ -251,11 +386,64 @@ class Cluster_3d:
 
 class Cluster:
     """
+            A class used to cluster pixel embeddings in 2D
 
-    """
+            Attributes
+            ----------
+            xym :  float (2, W, D)
+                    pixel coordinates of tile /grid
+
+            one_hot : bool
+                    Should be set to True, if the GT label masks are present in a one-hot encoded fashion
+
+            grid_x: int
+                    Length (width) of grid / tile
+
+            grid_y: int
+                    Height of grid / tile
+
+            pixel_x: float
+                    if grid_x = 1000 and pixel_x = 1.0, then the pixel spacing along the x direction is pixel_x/grid_x = 1e-3
+            pixel_y: float
+                    if grid_y = 1000 and pixel_y = 1.0, then the pixel spacing along the y direction is pixel_y/grid_y = 1e-3
+
+
+            Methods
+            -------
+            __init__: Initializes an object of class `Cluster_3d`
+
+            cluster_with_gt: use the predicted spatial embeddings from all pixels belonging to the GT label mask
+                        to identify the predicted cluster (used during training and validation)
+
+            cluster:    use the  predicted spatial embeddings from all pixels in the test image.
+                        Employs `fg_thresh` and `seed_thresh`
+            cluster_local_maxima: use the  predicted spatial embeddings from all pixels in the test image.
+                        Employs only `fg_thresh`
+            """
 
     def __init__(self, grid_y, grid_x, pixel_y, pixel_x, one_hot=False):
 
+        """
+           Parameters
+           ----------
+           xym :  float (2, W, D)
+                    pixel coordinates of tile /grid
+
+            one_hot : bool
+                    Should be set to True, if the GT label masks are present in a one-hot encoded fashion
+
+            grid_x: int
+                    Length (width) of grid / tile
+
+            grid_y: int
+                    Height of grid / tile
+
+            pixel_x: float
+                    if grid_x = 1000 and pixel_x = 1.0, then the pixel spacing along the x direction is pixel_x/grid_x = 1e-3
+            pixel_y: float
+                    if grid_y = 1000 and pixel_y = 1.0, then the pixel spacing along the y direction is pixel_y/grid_y = 1e-3
+
+                   """
         xm = torch.linspace(0, pixel_x, grid_x).view(1, 1, -1).expand(1, grid_y, grid_x)
         ym = torch.linspace(0, pixel_y, grid_y).view(1, -1, 1).expand(1, grid_y, grid_x)
         xym = torch.cat((xm, ym), 0)
@@ -267,7 +455,23 @@ class Cluster:
         self.pixel_x = pixel_x
         self.pixel_y = pixel_y
 
-    def cluster_with_gt(self, prediction, instance, n_sigma=1, ):
+    def cluster_with_gt(self, prediction, gt_instance, n_sigma=2, ):
+        """
+            Parameters
+            ----------
+            prediction :  PyTorch Tensor
+                    Model Prediction (5, H, W)
+            gt_instance : PyTorch Tensor
+                    Ground Truth Instance Segmentation Label Map
+
+            n_sigma: int, default = 2
+                    Number of dimensions in Raw Image
+            Returns
+            ----------
+            instance: PyTorch Tensor (H, W)
+                    instance segmentation
+       """
+
         height, width = prediction.size(1), prediction.size(2)
 
         xym_s = self.xym[:, 0:height, 0:width]  # 2 x h x w
@@ -277,16 +481,16 @@ class Cluster:
 
         instance_map = torch.zeros(height, width).short().cuda()
         if (self.one_hot):
-            unique_instances = torch.arange(instance.size(0))
+            unique_instances = torch.arange(gt_instance.size(0))
         else:
-            unique_instances = instance.unique()
+            unique_instances = gt_instance.unique()
             unique_instances = unique_instances[unique_instances != 0]
 
         for id in unique_instances:
             if (self.one_hot):
-                mask = instance[id].eq(1).view(1, height, width)
+                mask = gt_instance[id].eq(1).view(1, height, width)
             else:
-                mask = instance.eq(id).view(1, height, width)
+                mask = gt_instance.eq(id).view(1, height, width)
 
             center = spatial_emb[mask.expand_as(spatial_emb)].view(
                 2, -1).mean(1).view(2, 1, 1)  # 2 x 1 x 1
@@ -303,8 +507,32 @@ class Cluster:
 
         return instance_map
 
-    def cluster_local_maxima(self, prediction, n_sigma=2, fg_thresh=0.5, min_mask_sum=128, min_unclustered_sum=0,
+    def cluster_local_maxima(self, prediction, n_sigma=2, fg_thresh=0.5, min_mask_sum=0, min_unclustered_sum=0,
                              min_object_size=36):
+
+        """
+            Parameters
+            ----------
+            prediction :  PyTorch Tensor
+                    Model Prediction (5, H, W)
+            n_sigma: int, default = 2
+                    Number of dimensions in Raw Image
+            fg_thresh: float, default=0.5
+                    Foreground Threshold defines which pixels are considered to the form the Foreground
+                    and which would need to be clustered into unique objects
+            min_mask_sum: int
+                    Only start creating instances, if there are at least `min_mask_sum` pixels in foreground!
+            min_unclustered_sum: int
+                    Stop when the number of seed candidates are less than `min_unclustered_sum`
+            min_object_size: int
+                Predicted Objects below this threshold are ignored
+
+            Returns
+            ----------
+            instance: PyTorch Tensor (H, W)
+                    instance segmentation
+       """
+
         from scipy.ndimage import gaussian_filter
         height, width = prediction.size(1), prediction.size(2)
         xym_s = self.xym[:, 0:height, 0:width]
@@ -357,6 +585,31 @@ class Cluster:
 
     def cluster(self, prediction, n_sigma=2, seed_thresh=0.9, fg_thresh=0.5, min_mask_sum=0, min_unclustered_sum=0,
                 min_object_size=36):
+
+        """
+            Parameters
+            ----------
+            prediction :  PyTorch Tensor
+                    Model Prediction (5, H, W)
+            n_sigma: int, default = 2
+                    Number of dimensions in Raw Image
+            seed_thresh : float, default=0.9
+                    Seediness Threshold defines which pixels are considered to identify object centres
+            fg_thresh: float, default=0.5
+                    Foreground Threshold defines which pixels are considered to the form the Foreground
+                    and which would need to be clustered into unique objects
+            min_mask_sum: int
+                    Only start creating instances, if there are at least `min_mask_sum` pixels in foreground!
+            min_unclustered_sum: int
+                    Stop when the number of seed candidates are less than `min_unclustered_sum`
+            min_object_size: int
+                Predicted Objects below this threshold are ignored
+
+            Returns
+            ----------
+            instance: PyTorch Tensor (H, W)
+                    instance segmentation
+           """
 
         height, width = prediction.size(1), prediction.size(2)
         xym_s = self.xym[:, 0:height, 0:width]
